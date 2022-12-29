1ef9bdce2b436f35e8965df3e54953c1c7fe1d36
Eljakim Herrewijnen <eljakim.herrewijnen@gmail.com>
2022-12-29 20:58:10 +0100

    Added more projects and upload path for images

--- herreweb_backend/project/models.py
+++ herreweb_backend/project/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(default="No description added")
     status = models.IntegerField(default=ProjectStatus.RUNNING)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to="static/project_images/", null=True, blank=True)
     rtd_url = models.CharField(max_length=200, null=True, blank=True)
     url = models.CharField(max_length=200, null=True, blank=True)
     visible = models.BooleanField(default=False)
